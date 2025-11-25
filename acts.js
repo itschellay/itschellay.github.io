cb24d5170d3ba3ae43d58e64bd58de3dfaaeaef8
itschellay <rochelledvlsc@gmail.com>
2025-11-25 22:48:41 +0800

    works, and index modified, js file added

---
+++ acts.js
@@ -0,0 +1,14 @@
+var activities = [
+    {
+        "title": "Activity 4",
+        "description": "Create an HTML about any specific hardware device you want to showcase",
+        "repo": "redminote14pro5g",
+        "picture": "images/redmi.png"
+    },
+    {
+        "title": "Activity 5",
+        "description": "Create an HTML website about a place in the world you would like to showcase",
+        "repo": "philippines",
+        "picture": "images/philippines.png"
+    }
+]
