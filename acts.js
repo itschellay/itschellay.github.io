2641106a86c3d688bc259cedb0e96881ef8372d2
itschellay <rochelledvlsc@gmail.com>
2025-11-26 19:05:50 +0800

    added activity 6

--- acts.js
+++ acts.js
@@ -1,14 +1,20 @@
 var activities = [
     {
         "title": "Activity 4",
-        "description": "Create an HTML about any specific hardware device you want to showcase",
+        "description": "Create an HTML about any specific hardware device you want to showcase.",
         "repo": "redminote14pro5g",
         "picture": "images/redmi.png"
     },
     {
         "title": "Activity 5",
-        "description": "Create an HTML website about a place in the world you would like to showcase",
+        "description": "Create an HTML website about a place in the world you would like to showcase.",
         "repo": "philippines",
         "picture": "images/philippines.png"
+    },
+    {
+        "title": "Activity 6",
+        "description": "Create an HTML website that contains some form of gallery. It can be about anything.",
+        "repo": "gallery",
+        "picture": "images/gallery.png"
     }
 ]
