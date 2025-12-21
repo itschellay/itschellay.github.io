4bf54540033fb0afd756d0a42a3f2495d4688af4
Chelle <rochelledvlsc@gmail.com>
2025-12-22 06:00:39 +0800

    modified contacts

--- acts.js
+++ acts.js
@@ -3,24 +3,20 @@ var activities = [
         "title": "Activity 4",
         "description": "Create an HTML about any specific hardware device you want to showcase.",
         "repo": "redminote14pro5g",
-        "picture": "images/redmi.png"
     },
     {
         "title": "Activity 5",
         "description": "Create an HTML website about a place in the world you would like to showcase.",
         "repo": "philippines",
-        "picture": "images/philippines.png"
     },
     {
         "title": "Activity 6",
         "description": "Create an HTML website that contains some form of gallery. It can be about anything.",
         "repo": "gallery",
-        "picture": "images/gallery.png"
     },
     {
         "title": "Activity 7",
         "description": "Create an HTML website that fetch data from open APIs.",
         "repo": "dragonballz",
-        "picture": "images/dragonballz.png"
     }
 ]
